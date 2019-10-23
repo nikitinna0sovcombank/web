f632bbbe1094a8b0d8c3228fe210967a40c63d5e
NikitaNikitin1 <54981849+NikitaNikitin1@users.noreply.github.com>
2019-10-23 23:53:59 +0400

    dsfs

--- src/main/java/app/configuration/ApplicationConfig.java
+++ src/main/java/app/configuration/ApplicationConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.view.JstlView;
 @ComponentScan("app")
 public class ApplicationConfig extends WebMvcConfigurerAdapter {
 
+    
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/WEB-INF/pages/**").addResourceLocations("/pages/");
